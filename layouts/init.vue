1f6a5084fe17a90b2d0bdf70ebb6522e7afae0d1
Taichi Iida <chiaki.621@gmail.com>
2022-03-28 14:38:16 +0900

    main

---
+++ layouts/init.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    />
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="fixed = !fixed"
+      >
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'InitLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: '初期設定'
+    }
+  }
+}
+</script>
---
+++ pages/insert.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <form id="insert_model" @submit.prevent>
+        <label>name<input v-model="data.name" name="name"></label>
+        <label>str<input v-model="data.str" name="str"></label>
+        <button @click="asyncPost()">
+          送信
+        </button>
+      </form>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'InsertTest',
+  data () {
+    return {
+      data: {
+        name: '',
+        str: ''
+      }
+    }
+  },
+  methods: {
+    async asyncPost () {
+      await this.$axios.$post('/api/test', this.data).then((_req, _res) => {
+        alert(this.data.name + 'は登録されました。')
+        this.$router.push('/hello_world')
+      })
+    }
+  }
+}
+</script>
